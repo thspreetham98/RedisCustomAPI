4ac09266aff712bee7ab0e3c59cba99554fa8c16
thspreetham98 <thspreetham@gmail.com>
2019-02-16 11:25:37 +0530

    Ping

---
+++ RedisCustomAPI/Controllers/ClientController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RedisCustomAPI.Services;
+
+namespace RedisCustomAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        public IDellServerService _service;
+        public ClientController(IDellServerService service)
+        {
+            this._service = service;
+        }
+
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            var result = _service.Ping();
+            return Ok(result);
+        }
+    }
+}
